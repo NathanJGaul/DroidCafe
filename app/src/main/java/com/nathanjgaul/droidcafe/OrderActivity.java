d6acf8bad71e0435d976c840b1fa8778194fa93d
Nathan Gaul <nathanjgaul@gmail.com>
2019-08-25 11:25:56 -0600

    Task 1 and 2

--- app/src/main/java/com/nathanjgaul/droidcafe/OrderActivity.java
+++ app/src/main/java/com/nathanjgaul/droidcafe/OrderActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -16,5 +19,31 @@ public class OrderActivity extends AppCompatActivity {
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.cart_message);
         textView.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.delivery_sameday_text));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.delivery_nextday_text));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.delivery_pickup_text));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
